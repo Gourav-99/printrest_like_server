ba082b9c37aa0d6be68239b4bd24277bfb01b922
Gourav <goupg1999@gmail.com>
2023-08-21 22:55:53 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(
